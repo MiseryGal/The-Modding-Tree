56f95d3aba4d6b663ee40f009d2dcb1688709968
MiseryGal <mugridgemaxwell@gmail.com>
2024-12-05 14:51:35 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -66,7 +66,7 @@ addLayer("energy", {
             title: "Power Saving",
             description: "Lowers the Energy Decay by 0.02.",
             tooltip: "New Formula: 10-(Energy*0.08)",
-            cost: new Decimal(300),
+            cost: new Decimal(280),
             unlocked() {
                 return hasUpgrade('energy', 14);
             },
