1956bb9aeec7f8a35f5ce0aecfced517e6745508
MiseryGal <mugridgemaxwell@gmail.com>
2024-12-05 05:29:09 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,9 +1,4 @@
 addLayer("energy", {
     name: "Energy", // The name of the layer
     symbol: "EN", // The symbol for the layer's node
-    position: 0, // Position of the layer in the tree
-    startData() { return {
-        unlocked: true,
-        points: new Decimal(1), // Starting energy points
-    }},
 });
