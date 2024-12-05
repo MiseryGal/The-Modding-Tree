3dbfa19bec7e47cc57ae0d4340c507273da62f93
MiseryGal <mugridgemaxwell@gmail.com>
2024-12-05 01:25:38 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,21 +1,20 @@
 addLayer("energy", {
-    name: "Energy",  
-    symbol: "EN",      
-    position: 0,     
-    startData() { 
-        return {
-            unlocked: true,               // Layer is available from the start
-            points: new Decimal(1),       // Starts with 1 Energy
-        }
-    },
-    color: "#FFD700", 
-    resource: "Energy", 
-    type: "none",   // No prestige mechanic
+    name: "Energy", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "EN", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#ebcc34",
+    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already hav
+    row: 0, // Row the layer is in on the tree (0 is the first row)
     upgrades: {
         11: {
             title: "Energy",
             description: "Begin Energy production.",
-            cost: new Decimal(1),         // Costs 1 Energy
-            }
+            cost: new Decimal(1),
         }
-});
+    },
+    layerShown(){return true}
+})
