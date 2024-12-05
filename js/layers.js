164d22a9271c8ea9d027428024e72507343dda5c
MiseryGal <mugridgemaxwell@gmail.com>
2024-12-05 02:05:53 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,12 +4,18 @@ addLayer("energy", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(1),
+		points: new Decimal(0),
     }},
     color: "#ebcc34",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
     type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     upgrades: {
         11: {
@@ -19,6 +25,8 @@ addLayer("energy", {
         },
     },
     tabFormat: [
+        ["display-text", function() { return `You have <b>${format(this.points)}</b> Energy.` }],
+        "blank",  // Adds space
         "upgrades",  // Displays the upgrades section
     ],
     layerShown(){return true}
