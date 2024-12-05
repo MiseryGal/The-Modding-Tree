f33a0c0d865dc7616bf2fefb1c7a2e877675bc85
MiseryGal <mugridgemaxwell@gmail.com>
2024-12-05 02:13:36 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,17 +4,15 @@ addLayer("energy", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(1),
     }},
     color: "#ebcc34",
     type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+    resource: "Energy", // Name of prestige currency
+    baseResource: "Energy", // Name of resource prestige is based on
+    baseAmount() {return player.points},
+    syncResources() {
+        this.points = player.points; // Sync resource with baseResource
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     upgrades: {
