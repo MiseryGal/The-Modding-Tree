e59dde9d0dcaac18eb333e1e2dcabe9c1047a310
MiseryGal <mugridgemaxwell@gmail.com>
2024-12-05 21:29:33 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -46,7 +46,7 @@ addLayer("energy", {
             title: "More Energy",
             description: "Adds 10 to the Energy base, Energy boosts Energy Points at a reduced rate.",
             tooltip: "Formula: Energy^0.9",
-            cost: new Decimal(95),
+            cost: new Decimal(0),
             unlocked() {
                 return hasUpgrade('energy', 11);
             },
@@ -55,7 +55,7 @@ addLayer("energy", {
             title: "These are useful now",
             description: "Energy Points boost Energy Base at a reduced rate.",
             tooltip: "Formula: Energy Points^0.2",
-            cost: new Decimal(195),
+            cost: new Decimal(0),
             unlocked() {
                 return hasUpgrade('energy', 12);
             },
@@ -63,7 +63,7 @@ addLayer("energy", {
         14: {
             title: "Something new",
             description: "Unlocks an Energy buyable.",
-            cost: new Decimal(250),
+            cost: new Decimal(0),
             unlocked() {
                 return hasUpgrade('energy', 13);
             },
@@ -72,7 +72,7 @@ addLayer("energy", {
             title: "Power Saving",
             description: "Lowers the Energy Decay by 0.02.",
             tooltip: "New Formula: 10-(Energy*0.08)",
-            cost: new Decimal(320),
+            cost: new Decimal(0),
             unlocked() {
                 return hasUpgrade('energy', 14);
             },
@@ -80,7 +80,7 @@ addLayer("energy", {
         22: {
             title: "Optimizations",
             description: "Multiplies Energy Points by 10 and adds a further 10 to the Energy base.",
-            cost: new Decimal(510),
+            cost: new Decimal(0),
             unlocked() {
                 return hasUpgrade('energy', 21);
             },
@@ -88,7 +88,7 @@ addLayer("energy", {
         23: {
             title: "Bored?",
             description: "1.2x Energy Base",
-            cost: new Decimal(357),
+            cost: new Decimal(0),
             unlocked() {
                 return hasUpgrade('energy', 22);
             },
